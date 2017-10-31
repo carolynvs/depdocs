8174a8d726cab9f088c0d1f3f3519472f15a5b80
Carolyn Van Slyck <me@carolynvanslyck.com>
2017-10-31 15:20:26 -0500

    debug info during preview is helpful

--- scripts/depdocs.sh
+++ scripts/depdocs.sh
@@ -1,13 +1,11 @@
 DOCS=`pwd`/docs
 
-echo $DOCS
-
 hugo () {
   docker run --rm -it -v $DOCS:/var/www -w /var/www -p 1313:1313 carolynvs/hugo:0.30.2 $@
 }
 
 preview() {
-  hugo server --baseUrl=http://localhost --bind=0.0.0.0
+  hugo server --debug --baseUrl=http://localhost --bind=0.0.0.0 -d ./_preview
 }
 
 generate() {
